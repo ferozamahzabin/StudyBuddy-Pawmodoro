9e71aa6e7093b248929c2d85f2de4907fe3f1f52
Anika2214 <anika.tahsin8@g.bracu.ac.bd>
2025-05-17 04:08:35 +0600

    Delete public/routes directory

--- public/routes/settingsRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controller/settingsController');
-
-router.post('/', controller.saveUserSettings);
-router.get('/', controller.getUserSettings);
-
-module.exports = router;
